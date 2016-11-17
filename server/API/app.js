17ea03aa392c8ff891913ad52458ba5121826bd3
Scott <scolark27@gmail.com>
2016-11-17 23:47:34 +0000

    added files

---
+++ server/API/app.js
@@ -0,0 +1,16 @@
+ 
+ (function(){
+
+     'use strict'
+
+     var express = require('express');
+     var app = express();
+
+
+     app.listen(3000, function(){
+
+         console.log('listening on port 3000');
+
+     });
+
+ })()
