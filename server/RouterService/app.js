3cf8e4e24e0c5308fd9145f13d283733320d4d28
Scott <scolark27@gmail.com>
2016-11-22 23:04:27 +0000

    restructure

--- server/RouterService/app.js
+++ server/RouterService/app.js
@@ -34,22 +34,29 @@
 
     amqp.connect('amqp://localhost', function(err, conn) {
         conn.createChannel(function(err, ch) {
-            var q = 'getAvailableCanvasService';
 
-            ch.assertQueue(q, {durable: false});
-            ch.prefetch(1);
-            console.log(' [x] Awaiting RPC requests');
-            
-            ch.consume(q, function reply(msg) {
+            externalip(function (err, ip) {
+                console.log(ip);
+                console.log(err);
+                var q = 'getAvailableCanvasService';
 
-                var r = ip + ':' + port;
-                console.log(r);
-                ch.sendToQueue(msg.properties.replyTo,
-                    new Buffer(r.toString()),
-                    {correlationId: msg.properties.correlationId});
+                ch.assertQueue(q, {durable: false});
+                ch.prefetch(1);
+                console.log(' [x] Awaiting RPC requests');
+                
+                ch.consume(q, function reply(msg) {
 
-                ch.ack(msg);
+                    var r = ip + ':' + port;
+                    console.log(r);
+                    ch.sendToQueue(msg.properties.replyTo,
+                        new Buffer(r.toString()),
+                        {correlationId: msg.properties.correlationId});
+
+                    ch.ack(msg);
+                });
+
             });
+            
         });
     });
     
